61156da799a69bc69159eac9b9b4760e450ab19a
Michael Stephens <me@mikej.st>
2009-11-17 18:36:26 -0500

    better testing of mongo.Insert/mongo.Query

--- mongo_test.go
+++ mongo_test.go
@@ -11,18 +11,24 @@ import (
 )
 
 func TestInsert(t *testing.T) {
-	obj, _ := mongo.BytesToBSON([]byte{92, 0, 0, 0, 1, 115, 101, 99, 111, 110, 100, 0, 0, 0, 0, 0, 0, 0, 0, 64, 3, 102, 105, 102, 116, 104, 0, 23, 0, 0, 0, 2, 118, 0, 2, 0, 0, 0, 101, 0, 2, 102, 0, 2, 0, 0, 0, 105, 0, 0, 3, 102, 111, 117, 114, 116, 104, 0, 5, 0, 0, 0, 0, 2, 116, 104, 105, 114, 100, 0, 6, 0, 0, 0, 116, 104, 114, 101, 101, 0, 16, 102, 105, 114, 115, 116, 0, 1, 0, 0, 0, 0});
+	obj, ok := mongo.BytesToBSON([]byte{92, 0, 0, 0, 1, 115, 101, 99, 111, 110, 100, 0, 0, 0, 0, 0, 0, 0, 0, 64, 3, 102, 105, 102, 116, 104, 0, 23, 0, 0, 0, 2, 118, 0, 2, 0, 0, 0, 101, 0, 2, 102, 0, 2, 0, 0, 0, 105, 0, 0, 3, 102, 111, 117, 114, 116, 104, 0, 5, 0, 0, 0, 0, 2, 116, 104, 105, 114, 100, 0, 6, 0, 0, 0, 116, 104, 114, 101, 101, 0, 16, 102, 105, 114, 115, 116, 0, 1, 0, 0, 0, 0});
+	assertTrue(ok, "failed parsing BSON obj", t);
 
-	conn, _ := mongo.Connect("localhost", 27017);
+	conn, err := mongo.Connect("127.0.0.1", 27017);
+	assertTrue(err == nil && conn != nil, fmt.Sprintf("failed connecting to mongo: %v", err), t);
+
 	coll := conn.GetDB("test").GetCollection("coll");
 	coll.Insert(obj);
 
-	q, _ := mongo.BytesToBSON([]byte{5, 0, 0, 0, 0});
+	q, _ := mongo.Marshal(map[string]string{});
 	ret, err := coll.Query(q);
-	if err != nil || ret == nil {
-		fmt.Printf("Failed: %v\n", err)
-	}
+	assertTrue(err == nil && ret != nil, "query succeeded", t);
 
-	doc, err := ret.GetNext();
-	fmt.Printf("First doc: %v\n", doc.Kind());
+	doc, _ := ret.GetNext();
+	assertTrue(doc.Kind() == mongo.ObjectKind, "query returned document", t);
+	assertTrue(doc.Get("first").Int() == 1, "returned doc has proper 'first' element", t);
+	assertTrue(doc.Get("second").Number() == 2, "returned doc has proper 'second' element", t);
+	assertTrue(doc.Get("third").String() == "three", "returned doc has proper 'third' element", t);
+	assertTrue(doc.Get("fourth").Kind() == mongo.ObjectKind, "returned doc has proper 'fourth' element", t);
+	assertTrue(doc.Get("fifth").Get("f").String() == "i" && doc.Get("fifth").Get("v").String() == "e", "returned doc has proper 'fifth' element", t);
 }
